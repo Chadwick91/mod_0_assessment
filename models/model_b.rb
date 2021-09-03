ab74270e34d87146438110c3d167dffb66d4b5cb
Chad Morger <chadmorger@gmail.com>
2021-09-03 08:56:14 -0600

    Add developer hash model

---
+++ models/model_b.rb
@@ -0,0 +1,12 @@
+# Developer Model
+
+developer = {
+  coding_language => "Ruby",
+  program => "Backend",
+  age => 32,
+  years_of_experience => 3,
+  is_genious => false,
+  is_resourceful => true,
+  variables_used => ["String", "Integer", "Float", "Boolean", "Array", "Hash"],
+  language_stack => ["Ruby", "Python", "JavaScript", "C++""]
+}
