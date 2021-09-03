cd0dd7636c322ee437804052ed603919a776c487
Chad Morger <chadmorger@gmail.com>
2021-09-03 08:44:11 -0600

    Add dinosaur hash model

---
+++ models/model_a.rb
@@ -0,0 +1,12 @@
+# Dinosaur Model
+
+dinosaur = {
+  name => "Velociraptor",
+  time_existed => "Cretaceous period",
+  number_of_teeth => 30,
+  speed_in_mph => 40,
+  is_predator => true,
+  is_large => false,
+  distinctive_physical_traits => ["sharp claws", "long snout", "concave skull"],
+  prey => ["reptiles", "insects", "small mammals"]
+}
