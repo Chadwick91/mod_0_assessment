87695046b146fd90eb98818be0726c39477a0b81
Chad Morger <chadmorger@gmail.com>
2021-09-03 08:29:09 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,16 @@
+movie = "Dumb and Dumber"
+
+is_entertaining = true
+
+movie = "Happy Gilmore"
+
+num_4 = 20
+num_6 = 9
+
+product = num_4 * num_6
+
+favorite_foods = ["Green Chili", "Tacos", "Buffalo Chicken Sandwich"]
+
+favorite_foods.shift # using shift method to drop first element "Green Chili" from array
+
+movie.length # .length will tell me how many characters are in the "movie" string including spaces
